353dd5d8e99216d871afc777fd04232d673afde9
wigfyehkdwi <amoigus42069@hotmail.com>
2023-05-20 12:50:36 -0400

    farded

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float moveSpeed = 600f;
+    private float movement = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1; // ? Is this necesary?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Time.timeScale = 0;
+    }
+}
